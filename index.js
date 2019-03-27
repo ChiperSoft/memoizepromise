296ad1c4ba883b2752c984bdd4c095236df4e277
Jocelyn Badgley (Twipped) <joc@twipped.com>
2019-03-27 14:23:45 -0700

    These syntaxes don’t work on node 4

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var memoize = require('memoizesync');
 module.exports = exports = function memoizePromise (fn, options) {
 	var memoizedFn = memoize(fn, options);
 	var wrapper = function () {
-		var args = Array.from(arguments);
+		var args = Array.prototype.slice.call(arguments);
 		var p = memoizedFn.apply(this, args);
 
 		if (!p || typeof p.then !== 'function') {
 			return p;
 		}
 
-		p.then(null, () => memoizedFn.purge.apply(memoizedFn, args));
+		p.then(null, function () { memoizedFn.purge.apply(memoizedFn, args); });
 
 		return p;
 	};
